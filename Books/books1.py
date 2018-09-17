a81781ee67fc246501f06090248b10de05b5772e
Conor Gormally <ConorGormally@eduroam-163-46.dyn.carleton.edu>
2018-09-16 19:49:55 -0500

    adding books folder

--- Books/books1.py
+++ Books/books1.py
@@ -1,1 +1,17 @@
-print("we like books")
+import csv
+import sys
+
+
+def books1():
+    fileName = sys.argv[1]
+    allBookData = []
+    with open('books.csv', newline='') as f:
+        fileReader = csv.reader(f)
+        print('test')
+        for row in fileReader:
+            #allBookData.append(parseBookData(row))
+            print(row)
+books1()
+# def parseBookData(titleYearAuthor)
+#    singleBookData = []
+#    singleBookData = titleYearAuthor.split(",")
